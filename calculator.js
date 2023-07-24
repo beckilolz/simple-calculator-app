4e9e294cbe1a11913561c065a4399de0a10a0321
Luciano Suen <34027007+LSOffice@users.noreply.github.com>
2023-07-24 08:19:07 +0000

    Commit

--- calculator.js
+++ calculator.js
@@ -4,16 +4,28 @@ Number.prototype.countDecimals = function () {
 }
 
 function buttonClicked() {
+    document.getElementById('happy-frog').style.display = 'none';
+    document.getElementById('sad-cat').style.display = 'none';
+    document.getElementById("operation").innerHTML = "";
     const operations = ["+", "-", "*", "/", "**", "sqrt", "log"];
     const num1 = parseFloat(prompt("What is your first number?").replaceAll(" ", ""));
     const operand = prompt("What is your operand? (+, -, *, /, **, sqrt, log)").replaceAll(" ", "")
-    if (!operations.includes(operand)) throw new Error("This operand (" + operand + ") is not valid!");
-    let num2 = 0;
+    if (!operations.includes(operand)) {
+        document.getElementById('sad-cat').style.display = 'inline';
+        throw new Error("This operand (" + operand + ") is not valid!");
+    }
+    let num2 = 10;
     if (!(operand == "sqrt" ||operand == "log")){
         num2 = parseFloat(prompt("What is your second number?").replace(" ", ""));
     }
-    if (isNaN(num1) || isNaN(num2)) throw new Error("One or more of your numbers was not a number!");
-    if (num2 == 0 && operand == "/") throw new Error("This is impossible, you cannot divide by zero");
+    if (isNaN(num1) || isNaN(num2)) {
+        document.getElementById('sad-cat').style.display = 'inline';
+        throw new Error("One or more of your numbers was not a number!");
+    }
+    if (num2 == 0 && operand == "/") {
+        document.getElementById('sad-cat').style.display = 'inline';
+        throw new Error("This is impossible, you cannot divide by zero");
+    }
 
     let result;
     switch (operand) {
@@ -39,7 +51,10 @@ function buttonClicked() {
             result = Math.log10(num1);
             break;
     }
-    if (result == null) throw new Error("I don't know what happened")
+    if (result == null) {
+        document.getElementById('sad-cat').style.display = 'inline';
+        throw new Error("I don't know what happened")
+    }
     if (operand == "sqrt" || operand == "log") {
         let dp = 10;
         var decimals = result - Math.floor(result);
@@ -49,6 +64,17 @@ function buttonClicked() {
         }
         result = result.toFixed(decimalPlaces);
     }
-    
-    alert("Your answer is: " + result);
+
+    if (operand == "sqrt") {
+        document.getElementById("operation").innerHTML = operand + " " + num1 + " = " + result;
+    }
+    else {
+        document.getElementById("operation").innerHTML = num1 + " " + operand + " " + num2 + " = " + result;
+    }
+    document.getElementById('happy-frog').style.display = 'inline';
+}
+
+function script() {
+    document.getElementById('happy-frog').style.display = 'none';
+    document.getElementById('sad-cat').style.display = 'none';
 }
