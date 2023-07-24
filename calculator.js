687d03f8eeaf30b7d44683544ae38daf18125719
beckilolz <140365590+beckilolz@users.noreply.github.com>
2023-07-24 07:48:37 +0000

    Commit

--- calculator.js
+++ calculator.js
@@ -1,11 +1,11 @@
 
 
 const number1 = parseFloat(prompt('Enter first number: '));
-const operator = prompt('Enter operator ( either +, -, *, ^, ^2 or / ): ');
+const operator = prompt('Enter operator ( either +, -, *, /, sq, sqrt or log): ');
 
 
 let number2;
-if (operator != '^2') {
+if (operator != 'sq','sqrt','log') {
     number2 = parseFloat(prompt('Enter second number: '));
 }
 
@@ -23,17 +23,19 @@ else if (operator == '*') {
 else if (operator == '/') {
     result = number1 / number2;
 }
-else if (operator == '^') {
-    result = number1 ** number2;
-}
-else if (operator == '^2') {
+else if (operator == 'sq') {
     result = number1**2
 }
+else if (operator == 'sqrt') {
+    result = Math.sqrt(number1)
+}
+else if (operator == 'log') {
+    result = Math.log10(number1)
+}
 
-if (operator != '^2') {
+if (operator != 'sq','sqrt','log') {
     console.log(`${number1} ${operator} ${number2} = ${result}`);
 }
 
-else {console.log(`${number1} ${operator} = ${result}`);
+else if (operator == 'sq','sqrt','log') {console.log(`${number1} ${operator} = ${result}`);
 }
- 
